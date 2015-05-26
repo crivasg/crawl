e3da6e8b928099040565937034f5df4e220a7111
Cesar Rivas <crivasg@gmail.com>
2015-05-25 20:44:01 -0500

    Fixed format with go fmt. Check the length of the command line arguments. If less than one, print message and exit

--- crawl.go
+++ crawl.go
@@ -15,7 +15,12 @@ import (
 
 func main() {
 
-    flag.Parse()
-    args := flag.Args()
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
 
 }
